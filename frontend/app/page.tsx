4ead26d5cde22e5aa2d1ea16f128d43857b90d1d
Soham Ghugare <soham.ghugare@gmail.com>
2025-11-24 17:10:33 +0530

    feat: added hero section

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,12 @@
 import Navbar from "@/components/Navbar";
+import ChatInterface from "@/components/ChatInterface";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-black">
       <Navbar />
       <main className="min-h-screen">
-
+        <ChatInterface />
       </main>
     </div>
   );
